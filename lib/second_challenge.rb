c9e55bbc9b0a101a5e25e16512f302d861add02b
Robert M Ricci <rmricci121@gmail.com>
2019-12-18 01:01:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,7 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      patriarch =>{},
+      :patriarch =>{},
       :matriarch => {}, 
       :hero => {},
       :heroine_friends => []
